bf18163b709c47f1e0cc0801c993a067fa3854e3
Hannah Tiffany <hannahtiffany@icloud.com>
2024-04-15 12:16:33 -0700

    Sending username and timestamp to the host

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ var app = express();
 const timestamp = Date.now();
 const name = require("os").userInfo().username
 
+
 app.get("/url", (req, res, next) => {
- res.json([
-	 "message: My name is : timestamp: " ]);
+ res.send(["My name is", name, "timestamp", timestamp]);
 });
+
 app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
