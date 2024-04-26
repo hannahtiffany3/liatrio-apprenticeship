fe1bc8a33c55da52761375fe20795ff3d5e10477
Hannah Tiffany <hannahtiffany@icloud.com>
2024-04-26 13:30:06 -0700

    Fixing typo in actions.yml: DOCKER_PASSWORD to DOCKER_TOKEN

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const UNAME = require("os").userInfo().username
 
 name = "My name is: " + UNAME
 
-message = {"Message":name , "Timestamp": TIMESTAMP }
+message = {"Message": name , "Timestamp": TIMESTAMP }
 
 http.createServer(app).listen(3000)
 
@@ -19,6 +19,7 @@ app.get("/", (req, res, next) => {
 
 app.listen = (3000, 'localhost', function() {
 	console.log("Server running on port 3000")
+	console.log(UNAME);
 	const server = http.createServer(this)
 	return server.listen.apply(server, arguments)
 });
