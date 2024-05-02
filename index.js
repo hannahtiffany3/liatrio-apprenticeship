d97ab076fc62a2282e0d8c06623ee3ee02010cdc
Hannah Tiffany <hannahtiffany@icloud.com>
2024-05-01 20:12:05 -0700

    Testing

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ var timestamp = Date.now();
 var uname = require("os").userInfo().username
 name = "My name is: " + uname
 
-//var message = {"message": name, "timestamp": TIMESTAMP}
 
 http.createServer(app).listen(3000)
 
 app.get("/", (req, res, next) => {
  res.json({
 	 Message: name,
-	 Timestamp: timestamp
+	 Timestamp: timestamp,
+	 Test: "Test"
  });
 });
 
